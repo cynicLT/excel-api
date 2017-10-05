f5cda2e3e0efd76381223d863d0f5d6853e65e4f
Kiril Nugmanov <k.nugmanov@mail.com>
2017-10-05 08:20:29 +0300

    Added initial configuration

--- src/main/java/org/cynic/excel/ExcelApiApplication.java
+++ src/main/java/org/cynic/excel/ExcelApiApplication.java
@@ -3,10 +3,9 @@ package org.cynic.excel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class ExcelApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ExcelApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Configuration.class, args);
+    }
 }
